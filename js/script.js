706ac3bea424f3f950e55aafe75693af3fadc669
Lalit <lalitrajbanshi45@gmail.com>
2024-07-08 15:20:38 +0545

    updated-

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 let button = document.getElementById("menu-button");
-let element = document.querySelector(".site-navigation ul")
+let element = document.querySelector(".site-navigation")
 button.addEventListener("click",function(){
     element.classList.toggle("show");
     if(element.classList.contains("show")){
         button.innerHTML = "close"
+        // button.style.color = "white"
     }else{
         button.innerHTML = "menu"
+        // button.style.color = "#ff1964"
     }
 })
